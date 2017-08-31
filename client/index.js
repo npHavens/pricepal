de2e0f20d8b89e457d17caa137c6897eb5040162
Nick Havens <npahavens@gmail.com>
2017-08-30 19:50:47 -0500

    added App.jsx and Hello World test

--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
-console.log('TEST')
+console.log('Running index.js')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
