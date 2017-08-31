4671a1babaa2e382cf4084db4c8779d69eaa15a2
Nick Havens <npahavens@gmail.com>
2017-08-30 22:18:49 -0500

    added title, price, and add/remove favorites to list entries

--- client/components/FavoriteEntry.jsx
+++ client/components/FavoriteEntry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 let FavoriteEntry = () => (
-     <div className="favorite">Test Favorite</div>
+    <div className="favorite">
+      <a> Title </a>
+      <a> Lowest price </a>
+      <a> Remove from Favorites </a>
+    </div>
 );
 
 export default FavoriteEntry;
--- client/components/SearchResultEntry.jsx
+++ client/components/SearchResultEntry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 let SearchResultEntry = () => (
-     <div className="search-result">Test Search Result</div>
-);
+     <div className="search-result">
+      <a> Title</a>
+      <a> Price </a>
+      <a> Add To Favorites </a>
 
+     </div>
+);
 export default SearchResultEntry;
