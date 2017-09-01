0204e4a7d4d444ed5d2d18a1671025684b8e6a0b
Nick Havens <npahavens@gmail.com>
2017-08-31 22:08:49 -0500

    added sample data

--- server-config.js
+++ server-config.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', function(req, res) {
   //res.status(200).send()
   res.status(200).send('');
-
 })
+
 
 
 module.exports = app;
