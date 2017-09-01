8a93c4bb13eac9f1cc6ce440079586f99286692d
Nick Havens <npahavens@gmail.com>
2017-08-31 21:32:32 -0500

    added get method for index

--- server-config.js
+++ server-config.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(req, res) {
+  //res.status(200).send()
+  res.status(200).send('');
+
+})
+
+
 module.exports = app;
+
